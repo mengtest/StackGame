8e4b53cdcdadecaf523bd1549e242ffd78c04c4a
Taihyun Lee <th9061@hotmail.com>
2016-08-21 22:58:32 -0400

    adding the stack game

---
+++ Stack Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Text scoreText;
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+	// Use this for initialization
+	private void Start () {
+        scoreText.text = PlayerPrefs.GetInt("score").ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
